caf4e3bc0dd2b65c1d7db0eec02411afb01bb330
Vít Novotný <witiko@mail.muni.cz>
2019-09-05 15:21:50 +0200

    Set up indexing of authority bases

--- vufind-reindex.sh
+++ vufind-reindex.sh
@@ -4,7 +4,7 @@ print_info() {
   printf '%s:\t%s\n' "$(date --rfc-3339=seconds)" "$@"
 }
 
-usmarc_to_xml() {
+usmarc_to_xml_biblio() {
   java -jar import/lib/marc4j*.jar to_xml |
   sed -r '{
     # Remove invalid character references to ASCII C0 and C1 control codes, produced by marc4j.
@@ -15,43 +15,85 @@ usmarc_to_xml() {
   }'
 }
 
-download_dumps() {
+usmarc_to_xml_authority() {
+  java -jar import/lib/marc4j*.jar to_xml
+}
+
+download_dumps_biblio() {
   cd "$VUFIND_HOME"
   for BASENAME in mu-aleph-mub{01..02}.dump; do
-    # Download the Aleph database dump.
+    # Download the Aleph bibliography database dump.
     curl -L https://aleph.muni.cz/vufind-dump/${BASENAME}.mrc > ${BASENAME}.mrc.new
     mv ${BASENAME}.mrc{.new,}
     # Convert the database dump to XML and check that the XML is well-formed.
-    usmarc_to_xml < ${BASENAME}.mrc > ${BASENAME}.xml.new
+    usmarc_to_xml_biblio < ${BASENAME}.mrc > ${BASENAME}.xml.new
     xmllint --stream --noout - < ${BASENAME}.xml.new
     mv ${BASENAME}.xml{.new,}
   done
 }
 
-reindex_solr() {
+download_dumps_authority() {
+  cd "$VUFIND_HOME"
+  for BASENAME in mu-vufind-nkp10.dump; do
+    # Download the NKP authority database dump.
+    curl -L https://aleph.muni.cz/vufind-dump/${BASENAME}.mrc > ${BASENAME}.mrc.new
+    mv ${BASENAME}.mrc{.new,}
+    # Convert the database dump to XML and check that the XML is well-formed.
+    usmarc_to_xml_authority < ${BASENAME}.mrc > ${BASENAME}.xml.new
+    xmllint --stream --noout - < ${BASENAME}.xml.new
+    mv ${BASENAME}.xml{.new,}
+  done
+}
+
+download_dumps() {
+  download_dumps_biblio
+  download_dumps_authority
+}
+
+reindex_solr_biblio() {
   cd "$VUFIND_HOME"
   curl http://localhost:8080/solr/biblio/update --data "<delete><query>*:*</query></delete>" -H "Content-type:text/xml; charset=utf-8" |
   grep -qF '<int name="status">0</int>'
-  ./import-marc.sh <(head -n -1 mu-aleph-mub01.dump.xml && tail -n +2 mu-aleph-mub02.dump.xml)
+  ./import-marc.sh <(
+    head -n -1 mu-aleph-mub01.dump.xml
+    tail -n +2 mu-aleph-mub02.dump.xml
+  )
   sudo -u solr ./index-alphabetic-browse.sh
-  cd util
-  php optimize.php
 }
 
-print_info "Started reindexing Solr"
-(
-  set -e
+reindex_solr_authority() {
+  cd "$VUFIND_HOME"
+  curl http://localhost:8080/solr/authority/update --data "<delete><query>*:*</query></delete>" -H "Content-type:text/xml; charset=utf-8" |
+  grep -qF '<int name="status">0</int>'
+  ./import-marc-auth.sh <(
+    cat mu-vufind-nkp10.dump.xml
+  ) marc_auth_fast_topical.properties
+}
 
-  if [ -z "$VUFIND_HOME" ]
-  then
-    export VUFIND_HOME="$(cd "$(dirname "$0")" && pwd -P)"
+reindex_solr() {
+  reindex_solr_biblio
+  reindex_solr_authority
+  (cd util && php optimize.php)
+}
+
+main() {
+  print_info "Started reindexing Solr"
+  (
+    set -e
+
     if [ -z "$VUFIND_HOME" ]
     then
-      exit 1
+      export VUFIND_HOME="$(cd "$(dirname "$0")" && pwd -P)"
+      if [ -z "$VUFIND_HOME" ]
+      then
+        exit 1
+      fi
     fi
-  fi
 
-  download_dumps
-  reindex_solr
-)
-print_info "Finished reindexing Solr with return code $?"
+    download_dumps
+    reindex_solr
+  )
+  print_info "Finished reindexing Solr with return code $?"
+}
+
+main

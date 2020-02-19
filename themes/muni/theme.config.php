032219fa1bfc801ced3e4ffada71c594f32433e4
Vít Novotný <witiko@mail.muni.cz>
2020-02-19 02:40:08 +0100

    List encodings in RecordDriver

--- themes/muni/theme.config.php
+++ themes/muni/theme.config.php
@@ -1,7 +1,15 @@
 <?php
 return [
     'extends' => 'bootstrap3',
+    'helpers' => [
+        'factories' => [
+            'Muni\View\Helper\Muni\RecordDataFormatter' => 'Muni\View\Helper\Muni\RecordDataFormatterFactory',
+        ],
+        'aliases' => [
+            'recordDataFormatter' => 'Muni\View\Helper\Muni\RecordDataFormatter',
+        ]
+    ],
     'css' => [
-            'muni.css'
+        'muni.css'
     ]
 ];

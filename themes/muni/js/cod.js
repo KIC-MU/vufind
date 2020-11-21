732fb38a778ab46ca02a6467d2815b6c6dcd6071
Vít Novotný <witiko@mail.muni.cz>
2020-11-21 02:04:11 +0100

    Add copy on demand

---
+++ themes/muni/js/cod.js
@@ -0,0 +1,16 @@
+function cod_doc_write(text) {
+  var cod = document.getElementById('cod');
+  cod.outerHTML = text;
+}
+
+function cod_tag(sysno) {
+  if (sysno.substring(0, 5) != 'MUB01') {
+    return;
+  }
+
+  sysno = sysno.replace(/^MUB0[1-3]/, '');
+  var url = 'https://newknihomol.phil.muni.cz/system/include/copy-available/' + sysno + '?url=katalog';
+  var script = document.createElement('script');
+  script.src = url;
+  document.body.appendChild(script);
+}
